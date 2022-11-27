ebceb42890aa8b40c0018f195ce128313f509f89
Connor Becker <connorbecker@Connors-MacBook-Pro-2015.local>
2022-11-26 21:24:09 -0500

    Front-end login auth

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
 import { IUser, CognitoService } from "src/app/common/cognito.service";
 
@@ -8,26 +9,55 @@ import { IUser, CognitoService } from "src/app/common/cognito.service";
   styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent implements OnInit, OnDestroy {
-  loading: boolean;
-  user: IUser;
+  signingIn : boolean;
+  registerForm: FormGroup;
+  submitted = false;
+  failed = false;
 
-  constructor(private router: Router, private cognitoService: CognitoService) {
-    this.loading = false;
-    this.user = {} as IUser;
+  constructor(
+    private router: Router,
+    private cognitoService: CognitoService,
+    private formBuilder: FormBuilder
+  ) {
+    this.signingIn  = false;
   }
 
-  public signIn(): void {
-    this.loading = true;
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      // name: ["", Validators.required],
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required, Validators.minLength(6)]],
+      // acceptTerms: [false, Validators.requiredTrue],
+    });
+  }
+  ngOnDestroy() {}
+
+  // convenience getter for easy access to form fields
+  get f() {
+    return this.registerForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    this.signIn(this.registerForm.value);
+  }
+
+  private signIn(user: IUser): void {
+    this.signingIn = true;
     this.cognitoService
-      .signIn(this.user)
+      .signIn(user)
       .then(() => {
         this.router.navigate(["/dashboard"]);
       })
       .catch(() => {
-        this.loading = false;
+        this.signingIn = false;
+        this.failed = true;
       });
   }
-
-  ngOnInit() {}
-  ngOnDestroy() {}
 }
